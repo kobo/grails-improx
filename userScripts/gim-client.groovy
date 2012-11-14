81b3f33427e6597246f2d6d98a0f1bf47dc1c4cf
Yasuharu NAKANO <ynak@jggug.org>
2012-11-14 10:39:37 +0900

    classized client sequences

--- userScripts/gim-client.groovy
+++ userScripts/gim-client.groovy
@@ -1,44 +1,70 @@
-int DEFAULT_PORT = 8081
+//======================================
+// Grails Interactive-Mode Proxy Client
+//======================================
+
+class GimProxyClient {
+
+    private static final int DEFAULT_PORT = 8081
+    private Socket socket
+
+    def invoke(String command) {
+        validate(command)
+        connect(port)
+        send(command)
+        waitForResult()
+    }
+
+    private validate(String command) {
+        if (command.trim().empty) {
+            System.err.println "ERROR: Command not found."
+            System.exit 1
+        }
+    }
+
+    private getPort() {
+        (System.getProperty("interactive.proxy.port") ?: DEFAULT_PORT) as int
+    }
+
+    private connect(int port) {
+        try {
+            socket = new Socket("localhost", port)
+        } catch (ConnectException e) {
+            System.err.println "ERROR: Failed to connect to server via port $port."
+            System.err.println " Install grails-interactive-proxy plugin into your application"
+            System.err.println " and invoke start-interactive-proxy before connecting."
+            System.exit 1
+        }
+    }
+
+    private send(String command) {
+        socket << command << "\n"
+    }
+
+    private waitForResult() {
+        socket.withStreams { ins, out ->
+            while (true) {
+                def (text, eof) = readLine(ins)
+                println text
+                if (eof) {
+                    System.exit 0
+                }
+            }
+        }
+    }
+
+    private readLine(InputStream ins) {
+        def out = new ByteArrayOutputStream()
+        int ch
+        while ((ch = ins.read()) != -1) {
+            if (ch == '\n') { // LF (fixed)
+                return [out.toString(), false] // 2nd arg is EOF flag
+            }
+            out.write((byte) ch)
+        }
+        return [out.toString(), true] // 2nd arg is EOF flag
+    }
+}
 
 String command = args.join(' ')
-if (command.trim().empty) {
-    System.err.println "ERROR: Command not found."
-    System.exit 1
-}
+new GimProxyClient().invoke(command)
 
-int port = (System.getProperty("interactive.proxy.port") ?: DEFAULT_PORT) as int
-def socket
-try {
-    socket = new Socket("localhost", port)
-} catch (ConnectException e) {
-    System.err.println "ERROR: Failed to connect to server via port $port."
-    System.err.println " Install grails-interactive-proxy plugin into your application"
-    System.err.println " and invoke start-interactive-proxy before connecting."
-    System.exit 1
-}
-
-// Send command
-socket << command << "\n"
-
-// Output result
-socket.withStreams { ins, out ->
-    while (true) {
-        def (text, eof) = readLine(ins)
-        println text
-        if (eof) {
-            System.exit 0
-        }
-    }
-}
-
-def readLine(InputStream ins) {
-    def out = new ByteArrayOutputStream()
-    int ch
-    while ((ch = ins.read()) != -1) {
-        if (ch == '\n') { // LF (fixed)
-            return [out.toString(), false] // 2nd arg is EOF flag
-        }
-        out.write((byte) ch)
-    }
-    return [out.toString(), true] // 2nd arg is EOF flag
-}
