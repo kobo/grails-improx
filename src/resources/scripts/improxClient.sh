c0331498ef0eb7d216e0f00cca9126122234c2f2
Yasuharu NAKANO <ynak@jggug.org>
2012-11-22 10:34:51 +0900

    added client shell scripts

---
+++ src/resources/scripts/improxClient.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#
+# Usage: improxClient.sh <COMMAND_LINE>
+#
+# Examples:
+#     improxClient.sh help
+#     improxClient.sh test-app unit: sample.SampleUnitTests
+#     env IMPROX_PORT=8888 improxClient.sh help
+#
+
+#---------------------------------------
+# Config
+#
+
+DEFAULT_PORT=8081
+
+#---------------------------------------
+# Definition
+#
+
+error() {
+    local message="$*"
+    echo "ERROR: $message" 1>&2
+}
+
+die() {
+    local message="$*"
+    error "$message"
+    exit 1
+}
+
+call_improx() {
+    local port="$1"
+    shift
+    local command="$*"
+    echo "Executing '${command}' via interactive-mode proxy..."
+    echo "$command" | nc localhost $port
+    [ $? -eq 0 ] || die "\
+Failed to connect to server via port $port.
+  Before connecting, install 'improx' plugin into your application, and
+  run the 'improx-start' command on interactive-mode of the application."
+}
+
+usage() {
+    echo "usage: `basename $0` <COMMAND>" >&2
+    echo "examples:"
+    echo "  $ `basename $0` help" >&2
+    echo "  $ improxClient.sh test-app unit: sample.SampleUnitTests" >&2
+    echo "  $ env IMPROX_PORT=8888 `basename $0` help" >&2
+}
+
+#---------------------------------------
+# Main
+#
+
+if [ $# -eq 0 ]; then
+    usage
+    exit 1
+fi
+
+port=${IMPROX_PORT:=$DEFAULT_PORT}
+call_improx $port $*
+
