4e5f341d30a8551975080516684007dd78294d11
Yasuharu NAKANO <ynak@jggug.org>
2012-12-05 09:26:59 +0900

    upgraded rest-client-builder plugin to 1.0.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(':release:2.2.0', ':rest-client-builder:1.0.2') {
+        build(':release:2.2.0', ':rest-client-builder:1.0.3') {
             export = false
         }
     }
