a32c15769b5db013f1dfbe00c69061d146607445
Yasuharu NAKANO <ynak@jggug.org>
2012-12-05 09:25:52 +0900

    upgraded release plugin to 2.2.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,16 +17,16 @@ grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
 
-	inherits 'global'
-	log 'warn'
+    inherits 'global'
+    log 'warn'
 
-	repositories {
-		grailsCentral()
-	}
+    repositories {
+        grailsCentral()
+    }
 
-	plugins {
-		build(':release:2.1.0', ':rest-client-builder:1.0.2') {
-			export = false
-		}
-	}
+    plugins {
+        build(':release:2.2.0', ':rest-client-builder:1.0.2') {
+            export = false
+        }
+    }
 }
